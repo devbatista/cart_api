dcd998a40ea2aa056373b60d2cc4062cf47c4aa0
Rafael Batista <rafael@devbatista.com>
2026-01-29 14:38:23 -0300

    criando validação na model CartItem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,16 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+  before_validation :set_prices
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
+
+  private
+
+  def set_prices
+    self.unit_price = product.price
+    self.total_price = unit_price.to_d * quantity.to_i
+  end
 end
