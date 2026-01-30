d085e7b32ebfcfa1c89c588e6ebb755cd47101ff
Rafael Batista <rafael@devbatista.com>
2026-01-30 16:03:46 -0300

    adicionando novos testes e traduzindo a descrição

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -37,4 +37,36 @@ RSpec.describe Cart, type: :model do
       expect(cart.last_interaction_at).to be_within(1.second).of(Time.current)
     end
   end
+
+  describe '#update_total_price!' do
+    it 'recalculates total_price by summing cart items total_price' do
+      cart = described_class.create!(total_price: 0, abandoned: false)
+      product1 = Product.create!(name: 'P1', price: 10.0)
+      product2 = Product.create!(name: 'P2', price: 5.0)
+
+      cart.cart_items.create!(product: product1, quantity: 2, unit_price: 10.0, total_price: 20.0)
+      cart.cart_items.create!(product: product2, quantity: 1, unit_price: 5.0, total_price: 5.0)
+
+      cart.update_total_price!
+      cart.reload
+
+      expect(cart.total_price).to eq(25.0)
+    end
+  end
+
+  describe 'scopes' do
+    it '.active returns only non-abandoned carts' do
+      active_cart = described_class.create!(total_price: 0, abandoned: false)
+      _abandoned_cart = described_class.create!(total_price: 0, abandoned: true)
+
+      expect(described_class.active).to contain_exactly(active_cart)
+    end
+
+    it '.abandoned returns only abandoned carts' do
+      _active_cart = described_class.create!(total_price: 0, abandoned: false)
+      abandoned_cart = described_class.create!(total_price: 0, abandoned: true)
+
+      expect(described_class.abandoned).to contain_exactly(abandoned_cart)
+    end
+  end
 end
